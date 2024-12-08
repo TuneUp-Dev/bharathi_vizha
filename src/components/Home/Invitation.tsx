b03c252a9b0364d9292ed58f68a9cd3fefd59363
Srini-10 <srinivasanp1018@gmail.com>
2024-12-08 14:20:50 +0530

    feat: Invitation Added

---
+++ src/components/Home/Invitation.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Invitation = () => {
+  return (
+    <>
+      <div className="w-full h-auto py-20 px-4 flex flex-col justify-center items-center">
+        <h1 className="text-[32px] sm:text-[40px] lg:text-[50px] text-red-900 font-bold mb-7 text-center lg:text-left">
+          அழைப்பிதழ்
+        </h1>
+        <img
+          className="rounded-sm"
+          src="https://i.ibb.co/PcCvXYj/Whats-App-Image-2024-12-08-at-14-15-25.jpg"
+          alt=""
+        />
+      </div>
+    </>
+  );
+};
+
+export default Invitation;
