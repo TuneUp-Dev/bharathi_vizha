305c891530b2c65977a883830bbaf80a2f9a7af3
TuneUp Technologies <tuneup.devups@gmail.com>
2025-01-01 22:02:43 +0530

    Update FourthMain.tsx

--- src/components/Fourth/FourthMain.tsx
+++ src/components/Fourth/FourthMain.tsx
@@ -209,7 +209,7 @@ const FourthMain: React.FC = () => {
       </div>
 
       {/* Cards Grid */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 p-4 md:p-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-4 md:p-8">
         {cardData.map((card, index) => (
           <Card
             key={index}
